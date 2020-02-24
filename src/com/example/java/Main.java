2dd717c8e6d352571ad6fd1e1e21cefaf50c36a9
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-24 13:34:48 -0500

    create looping code block

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -1,49 +1,81 @@
 package com.example.java;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+////        If / else
+//        int monthNumber = 6;
+//
+//        if (monthNumber >= 0 && monthNumber <= 2){
+//            System.out.println("It's the first Quarter");
+//        } else if (monthNumber >= 3 && monthNumber <= 5) {
+//            System.out.println("It's the second Quarter");
+//        } else {
+//            System.out.println("You are in the second half of the year");
+//        }
+//
+////        Switch Statement
+//        int month = 0;
+//
+//        switch (month) {
+//            case 0:
+//                System.out.println("It's January");
+//                break;
+//            case 1:
+//                System.out.println("It's February");
+//                break;
+//            case 2:
+//                System.out.println("It's March");
+//                break;
+//            default:
+//                System.out.println("It's some other month");
+//        }
+//
+//        String monthName = "January";
+//        switch (monthName){
+//            case "January":
+//                System.out.println("It's the first month");
+//                break;
+//
+//            default:
+//                System.out.println("It isn't January");
+//        }
+//}
+    private static final String[] months = {
+        "January", "February", "March", "April",
+        "May", "June", "July", "August", "September",
+        "October", "November", "December"
 
-//        If / else
-        int monthNumber = 6;
+    };
+    public static void main(String[] args){
+//        for loop
 
-        if (monthNumber >= 0 && monthNumber <= 2){
-            System.out.println("It's the first Quarter");
-        } else if (monthNumber >= 3 && monthNumber <= 5) {
-            System.out.println("It's the second Quarter");
-        } else {
-            System.out.println("You are in the second half of the year");
-        }
+//        for (int i = 0; i <months.length ; i++) {
+//            System.out.println(months[i]);
+//        }
 
-//        Switch Statement
-        int month = 0;
+//        foreach loop
 
-        switch (month) {
-            case 0:
-                System.out.println("It's January");
-                break;
-            case 1:
-                System.out.println("It's February");
-                break;
-            case 2:
-                System.out.println("It's March");
-                break;
-            default:
-                System.out.println("It's some other month");
-        }
+//        for (String month:
+//             months) {
+//            System.out.println(month);
+//        }
 
-        String monthName = "January";
-        switch (monthName){
-            case "January":
-                System.out.println("It's the first month");
-                break;
+//        while loop
+//        int counter = 0;
+//        while(counter < months.length){
+//            System.out.println(months[counter]);
+//            counter ++;
+//        }
 
-            default:
-                System.out.println("It isn't January");
-        }
-
-
+//        do while loop
+        int counter = 0;
+        do {
+            System.out.println(months[counter]);
+         counter ++;
+        }while(counter < months.length);
 
     }
 }
