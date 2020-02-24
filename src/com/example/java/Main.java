a3928a965461b6684c668e0b2c183eaec5b2ae1b
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-24 14:13:43 -0500

    methods

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -4,7 +4,24 @@ package com.example.java;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        String s1 = getInput("Enter value 1: ");
+        String s2 = getInput("Enter value 2: ");
+        double result = addValues(s1, s2);
+        System.out.println("The result is: "+ result);
+    }
 
+    private static String getInput(String prompt) {
+        System.out.print(prompt);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static double addValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 + d2;
+    }
 //    public static void main(String[] args) {
 //
 ////        If / else
@@ -45,13 +62,13 @@ public class Main {
 //                System.out.println("It isn't January");
 //        }
 //}
-    private static final String[] months = {
-        "January", "February", "March", "April",
-        "May", "June", "July", "August", "September",
-        "October", "November", "December"
-
-    };
-    public static void main(String[] args){
+//    private static final String[] months = {
+//        "January", "February", "March", "April",
+//        "May", "June", "July", "August", "September",
+//        "October", "November", "December"
+//
+//    };
+//    public static void main(String[] args){
 //        for loop
 //        To make this code reusable I'll extract that code to a new method.
 //        IntelliJ IDEA has tools that let you do this easily. It's a process called refactor.
@@ -61,22 +78,22 @@ public class Main {
 //         It loops through the list of months, matches the value that was passed in, and if found,
 //         returns the equivalent number or returns a value of minus one if that number isn't found.
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Enter a month: ");
-            String monthNAme = scanner.nextLine();
-            if(monthNAme.length() == 0){
-                break;
-            }
-            int monthNumber = getMonthNumber(monthNAme);
-            if (monthNumber == -1) {
-                System.out.println("Month not found");
-            } else {
-                System.out.println(String.format(
-                        "%s is month number %d", monthNAme, monthNumber + 1
-                ));
-            }
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            System.out.print("Enter a month: ");
+//            String monthNAme = scanner.nextLine();
+//            if(monthNAme.length() == 0){
+//                break;
+//            }
+//            int monthNumber = getMonthNumber(monthNAme);
+//            if (monthNumber == -1) {
+//                System.out.println("Month not found");
+//            } else {
+//                System.out.println(String.format(
+//                        "%s is month number %d", monthNAme, monthNumber + 1
+//                ));
+//            }
+//        }
 //        foreach loop
 
 //        for (String month:
@@ -101,19 +118,19 @@ public class Main {
 //         }
 //        }while(counter < months.length);
 
-    }
+//    }
 
-    private static void loopMonths() {
-        for (int i = 0; i <months.length ; i++) {
-            System.out.println(months[i]);
-        }
-    }
-    private static int getMonthNumber(String monthName) {
-        for (int i = 0; i <months.length ; i++) {
-            if (monthName.equalsIgnoreCase(months[i])) {
-                return i;
-            }
-        }
-        return -1;
-    }
+//    private static void loopMonths() {
+//        for (int i = 0; i <months.length ; i++) {
+//            System.out.println(months[i]);
+//        }
+//    }
+//    private static int getMonthNumber(String monthName) {
+//        for (int i = 0; i <months.length ; i++) {
+//            if (monthName.equalsIgnoreCase(months[i])) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
 }
